88bfc470d6f097d9a285c7d03f869802b88a3461
LeoZion <leozionsuporte@gmail.com>
2021-11-07 20:46:24 -0300

    component: results

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
+import Results from '../components/Results'
+import requests from '../utils/requests'
 
-export default function Home() {
+export default function Home({ results }) {
+  // console.log(results)
+
   return (
     <div>
       <Head>
@@ -13,15 +17,23 @@ export default function Home() {
       </Head>
       <Header />
       <Nav />
-
-      {/* NAV */}
-
-      <main>
-        <h1>Lets Build Hulu 2.0 Clone</h1>
-
-        {/* RESULTS */}
-
-      </main>
+      <Results results={results} />      
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json())
+
+  return {
+    props: {
+      results: request.results,
+    }
+  }
 }
