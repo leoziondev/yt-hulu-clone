170e36a85fa8dfb619d75f2a7233cafa9d036887
LeoZion <leozionsuporte@gmail.com>
2021-11-07 21:41:05 -0300

    custom responsive screen tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "2000px",
+      },
+    },
   },
   plugins: [require('tailwind-scrollbar-hide')],
 }
